97676dcee9dd3ccfad18d48721bb1e5ccf188121
kimsohea <kimsohea4425@gmail.com>
2023-01-17 16:14:30 +0900

    js01

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(function(){
 	let lineTotalLength = $(".cMLine>svg>polyline").get(0).getTotalLength()
 	console.log(lineTotalLength)
 	$(".cMLine>svg>polyline").css("stroke-dasharray", lineTotalLength)
+	let lineTop = $(`.cMLine>svg>polyline`).offset().top;
 
 	let scrollTop,strokeLength;
 	$(window).scroll(function(){
@@ -10,12 +11,11 @@ $(function(){
 
 		scrollTop = window.scrollY
 		if(scrollTop>500){
-			strokeLength= (scrollTop-500)*4 - 100;
+			strokeLength = scrollTop*3 - lineTop - 340;
 			$(`.cMLine polyline`).css(`stroke-dashoffset`,lineTotalLength - strokeLength);
 			console.log(lineTotalLength-(lineTotalLength - strokeLength) +"만큼그려짐");
-			if(strokeLength>372){
-
-			}
+			
 		}
 	});
+	console.log($(".cMLine polyline"));
 });
