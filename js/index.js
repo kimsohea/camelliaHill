0cd11d43ef0f96b4c3ac14079cbaf5a007973e17
kimsohea <kimsohea4425@gmail.com>
2023-01-18 18:09:01 +0900

    23.01.18.PM.06:08

--- js/index.js
+++ js/index.js
@@ -5,17 +5,16 @@ $(function(){
 
 	$(".cMLine>svg>polyline").css("stroke-dasharray", lineTotalLength)
 	$(".cMLine>svg>polyline").css("stroke-dashoffset", lineTotalLength)
-	let lineTop = $(`.cMLine>svg>polyline`).offset().top;
+	let lineTop = $(`.cMLine>svg`).offset().top;
 	let lineObj = $(`.cMLine>figure`).offset().top + $(`.cMLine>figure`).offset().left;
 	
 
 	let scrollTop,strokeLength,lineLength;
 	$(window).scroll(function(){
 		lineTotalLength = $(".cMLine>svg>polyline").get(0).getTotalLength()
-
 		scrollTop = window.scrollY
 		if(scrollTop > 500){
-			strokeLength = scrollTop*3.5 - lineTop - 585;
+			strokeLength = (scrollTop*2.95) - (lineTop*0.2 + 500);
 			$(`.cMLine polyline`).css(`stroke-dashoffset`,lineTotalLength - strokeLength);
 			//console.log(lineTotalLength-(lineTotalLength - strokeLength) +"만큼그려짐");
 		}
@@ -23,13 +22,26 @@ $(function(){
 		lineLength = lineTotalLength - (lineTotalLength - strokeLength);
 		$(`.cMLine>figure`).each(function(i){
 			let figureXYLength = null;
-			if($(this).hasClass("line")){
-				console.log($(this).height())
-				figureXYLength = ($(this).position().top+$(this).height())+$(this).position().left;
-
+			let distanceFigureX = null;
+			let distanceFigureY = null;
+			let figureHeight = $(this).height();
+			if($(this).position().left - $(this).prev().position().left<0){
+				distanceFigureX = -($(this).position().left - $(this).prev().position().left)
 			}else{
-				figureXYLength = $(this).position().top+$(this).position().left;
+				distanceFigureX = ($(this).position().left - $(this).prev().position().left)
 			}
+			if($(this).position().top - $(this).prev().position().top < 0){
+				distanceFigureY = -($(this).position().top - $(this).prev().position().top);
+			} else {
+				distanceFigureY = ($(this).position().top - $(this).prev().position().top);
+			}
+			let figureX = $(this).prev().position().left + distanceFigureX;
+			let figureY = $(this).prev().position().top + distanceFigureY;
+			if($(this).hasClass("line")){
+				figureY+=figureHeight
+			}
+			console.log(figureX +"+"+figureY)
+			figureXYLength = (figureY+figureX)*1.35;
 			
 			console.log("그려지는 줄의길이 : "+lineLength)
 			console.log(i+"번째 figure태그 좌표의 x,y합 : "+figureXYLength)
